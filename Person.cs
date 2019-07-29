e7fdcd9c2a11f0056a33480a21969e5c80d485e2
Mohamad <Mohamad@MOHAMAD>
2019-07-29 16:20:55 +0430

    added Person Class and its prototype.

---
+++ Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    [Serializable]
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public List<string> PhoneNumbers { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Person();
+        public override string ToString();
+        public TimeSpan TimeToBirthday();
+    }
+}
