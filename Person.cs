94238b78180a24134a50da58c4fbe20df4a6a6c8
Mohamad <Mohamad@MOHAMAD>
2019-07-30 11:51:27 +0430

    completed person class methods.

--- Person.cs
+++ Person.cs
@@ -16,8 +16,43 @@ namespace PhoneBook
         public List<string> PhoneNumbers { get; set; }
         public DateTime BirthDate { get; set; }
 
-        public Person();
-        public override string ToString();
-        public TimeSpan TimeToBirthday();
+        public Person()
+        {
+
+        }
+        public override string ToString()
+        {
+            // temp string to generate output
+            string result;
+
+            // add main details to result
+            result = string.Format("FirstName: '{0}' LastName: '{1}' Address: '{2}' Email: '{3}' BirthDate: '{4}'",
+                FirstName, LastName, Address, Email, BirthDate.ToShortDateString());
+
+            // add each phonenumber to result (if exists)
+            if (PhoneNumbers.Count > 0)
+            {
+                result += '\n';
+                result += "PhoneNumbers: ";
+                foreach(string item in PhoneNumbers)
+                {
+                    result += "'" + item + "',";
+                }
+            }
+
+            // skip to newline
+            result += '\n';
+
+            return result;
+        }
+        public TimeSpan TimeToBirthday()
+        {
+            TimeSpan result = DateTime.Today.Subtract(new DateTime(DateTime.Today.Year, BirthDate.Month, BirthDate.Day));
+            if (result.TotalHours > 0)
+            {
+                result = DateTime.Today.Subtract(new DateTime(DateTime.Today.AddYears(1).Year, BirthDate.Month, BirthDate.Day));
+            }
+            return result;
+        }
     }
 }
